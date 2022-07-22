e7c666de79460a9357e88ce962b369d6f76c8a37
quadiez <dduidokdoi@gmail.com>
2022-07-22 20:13:10 +0700

    fix: bingo item

--- db/models/BingoItemModel.js
+++ db/models/BingoItemModel.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose')
 
 const BingoItemSchema = new mongoose.Schema({
     mission: String,
-    isDone: Boolean,
-    location: String
+    location: String,
+    Image: String,
+    details: String
 })
 
 module.exports = mongoose.model.BingoItems || mongoose.model("BingoItems", BingoItemSchema);
